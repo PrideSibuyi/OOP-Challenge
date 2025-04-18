6492f7593455943b1821a376fe88b778f631be7f
Jdennismunene <dennismunene478@gail.com>
2025-04-18 13:20:19 +0300

    updated assignment

--- main.py
+++ main.py
@@ -36,7 +36,8 @@ def main():
             break
         else:
             print("Invalid choice. Please try again.")
+            
 
-            if __name__ == "__main__":
-                main()
+if __name__ == "__main__":
+    main()
 
